ce470fba1fd91250e8f46229eab2b85c38a14c49
BobbyDeluna <bobby.deluna2@gmail.com>
2022-11-03 16:10:14 -0700

    commit tree 2f09225fc4f6ba3178a96cf3a202b5f4f7f52442

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    last = self[-1]
+     if last == "."
+    true
+     else
+      false
+     end 
   end
 
   def question?
-
+    last = self[-1]
+    if last == "?"
+      true
+    else 
+      false
+    end
   end
 
   def exclamation?
-
+    last = self[-1]
+    if last == "!"
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+    sentence = self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count 
+    sentence
+    
   end
+
 end
